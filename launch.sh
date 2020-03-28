cb31d745b89f465a48b326ecb8ed968670500456
Yahya <youkharta@gmail.com>
2020-03-28 14:56:23 +0100

    update

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 docker rm -f con
 docker build . -t img
-docker run -it -p 3000:3000 -e HOST=3.82.213.251 --name con img:latest
+
+docker run -it -p 3000:3000 -e HOST=$(dig @resolver1.opendns.com ANY myip.opendns.com +short) --name con img:latest
